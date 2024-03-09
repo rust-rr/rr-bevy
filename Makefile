94678dfd95f4cf8d7c2850b12b76ee1b4aa54fb2
RenXusheng233 <782449909@qq.com>
2024-03-09 21:38:42 +0800

    chore: update

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 run:
 	cargo run --features bevy/dynamic_linking
+watch:
+	cargo watch -q -c -x 'run --features bevy/dynamic_linking'
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rust Invaders".into(),
+                resolution: (600., 666.).into(),
+                position: WindowPosition::At(IVec2::new(0, 400)), // FIXME: dev only
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_systems(Startup, startup_system)
+        .run();
+}
+
+fn startup_system(mut commands: Commands) {
+    // camera
+    commands.spawn(Camera2dBundle::default());
+
+    // add rectangle
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(150., 150.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
